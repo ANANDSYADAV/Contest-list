fa67c0ccf0b6f58d0e89938f275ca00ddf5ec8aa
ANAND YADAV <113358205+ANANDSYADAV@users.noreply.github.com>
2023-08-22 00:53:24 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ result.then((contest) => {
   cards = "";
   for (item in contest) {
     cards += `
-    <div class="card" style="width: 18rem;">
+    <div class="card">
     <img
       src="https://img.freepik.com/premium-vector/coder-developer-concentrated-working-project-developing-programming-coding-technologies_569013-336.jpg?w=2000"
       class="card-img-top" alt="Coding">
@@ -16,10 +16,10 @@ result.then((contest) => {
       <h5 id="card-title">${contest[item].site}</h5>
       <p class="text2">${contest[item].start_time}</p>
       <p class="text3">${contest[item].end_time}</p>
-      <a href="${contest[item].url}" class="btn btn-primary">Visit Contest Page</a>
+      <a href="${contest[item].url}" class="btn btn-primary">Visit Contest</a>
     </div>
   </div>
   `
-  }  
+  }
   cardcontainer.innerHTML = cards;
 })
