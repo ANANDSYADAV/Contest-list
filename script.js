2ef4dbf8e6b339bf6cd983fbcf805983291e1990
ANAND YADAV <113358205+ANANDSYADAV@users.noreply.github.com>
2023-08-21 22:49:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let url = "https://kontests.net/api/v1/all";
+
+let result = fetch(url);
+let cards;
+result.then((contest) => {
+  return contest.json();
+}).then((contest) => {
+  cards = "";
+  for (item in contest) {
+    cards += `
+    <div class="card" style="width: 18rem;">
+    <img
+      src="https://img.freepik.com/premium-vector/coder-developer-concentrated-working-project-developing-programming-coding-technologies_569013-336.jpg?w=2000"
+      class="card-img-top" alt="Coding">
+    <div class="card-body">
+      <h5 id="card-title">${contest[item].site}</h5>
+      <p class="text2">${contest[item].start_time}</p>
+      <p class="text3">${contest[item].end_time}</p>
+      <a href="${contest[item].url}" class="btn btn-primary">Visit Contest Page</a>
+    </div>
+  </div>
+  `
+  }  
+  cardcontainer.innerHTML = cards;
+})
