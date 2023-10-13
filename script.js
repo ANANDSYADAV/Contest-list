91202f7b1cf7be1766bd1cbe6ecdcb5dbbb74581
ANAND YADAV <113358205+ANANDSYADAV@users.noreply.github.com>
2023-10-13 17:31:17 +0530

    optimised the logic

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ let url = "https://kontests.net/api/v1/all";
 let result = fetch(url);
 let cards;
 result.then((contest) => {
-  return contest.json();
+    return contest.json();
 }).then((contest) => {
-  cards = "";
-  for (item in contest) {
-    cards += `
+    cards = "";
+    for (item in contest) {
+        cards += `
     <div class="card">
     <img
       src="https://img.freepik.com/premium-vector/coder-developer-concentrated-working-project-developing-programming-coding-technologies_569013-336.jpg?w=2000"
-      class="card-img-top" alt="Coding">
+      class="card-img-top" alt="Coding" id="cardimg">
     <div class="card-body">
       <h5 id="card-title">${contest[item].site}</h5>
       <p class="text2">${contest[item].start_time}</p>
-      <p class="text3">${contest[item].end_time}</p>
+      <p class="text2">${contest[item].end_time}</p>
       <a href="${contest[item].url}" class="btn btn-primary">Visit Contest</a>
     </div>
   </div>
   `
-  }
-  cardcontainer.innerHTML = cards;
+    }
+    cardcontainer.innerHTML = cards;
 })
